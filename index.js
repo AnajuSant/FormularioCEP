805dbc0aa655fd5b2f1d00eed9699af0a13805c3
AnajuSant <97199596+AnajuSant@users.noreply.github.com>
2022-05-19 17:29:00 -0300

    Cep

---
+++ index.js
@@ -0,0 +1,26 @@
+function cep() {
+    let local = document.getElementById("Cep").value;
+    let regex = /[0-9]{8}/g;
+
+    if (local.match(regex)) {
+        document.getElementById("Cep").classList.remove("balao-vermelho");
+    } else {
+        document.getElementById("balaored").style.display = "block";
+        document.getElementById("Cep").classList.add("balao-vermelho");
+        return false;
+    }
+    fetch(`http://viacep.com.br/ws/${local}/json/`)
+        .then((response) => {
+            return response.json();
+            cep;
+        })
+        .then((dados) => {
+            console.log(dados);
+            document.getElementById("rua").value = dados.logradouro;
+            document.getElementById("Bairro").value = dados.bairro;
+            document.getElementById("Cidade").value = dados.localidade;
+            document.getElementById("UF").value = dados.uf;
+            document.getElementById("IBGE").value = dados.ibge;
+            document.getElementById("DDD").value = dados.ddd;
+        });
+}
